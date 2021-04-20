0c893815c3f2bc8432608ff2ac1bc3504946b418
Kintis Alexandros <kintisalexandros@gmail.com>
2021-04-21 01:02:17 +0300

    Working much more structured

--- Server/src/main/java/Server/App.java
+++ Server/src/main/java/Server/App.java
@@ -1,54 +1,19 @@
 package Server;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.spi.LoggerContext;
-import org.apache.logging.log4j.simple.SimpleLoggerContext;
-
+import java.util.ArrayList;
 
 public class App{
 
-	private static Logger log = LogManager.getLogger(App.class);
-
-	public static enum LogLevel {
-		DEBUG,
-		FATAL,
-		ERROR,
-		WARN,
-		INFO
-	}
+	private static ArrayList<VideoFile> files;
+    public static final String folderName = "videos";
 
 	public static void main(String[] args) {
-		VideoHandler handler = new VideoHandler();
 
-		for(var i : handler.getVideoFiles().keySet()) {
-			System.out.println(i + " " + handler.getVideoFiles().get(i));
-		}
+		FilesHandler handler = new FilesHandler();
+		files = handler.getFiles();
 
 	}
 
-	public static Logger log(LogLevel logLevel, String message) {
-		switch(logLevel) {
-			case DEBUG:
-				log.debug(message);
-				break;
-			case FATAL:
-				log.fatal(message);
-				break;
-			case ERROR:
-				log.error(message);
-				break;
-			case WARN:
-				log.warn(message);
-				break;
-			case INFO:
-				log.info(message);
-				break;
-		default:
-			log.error("Wrong logger type");
-			System.exit(1);
-		}
-		return log;
-	}
+
 
 }
