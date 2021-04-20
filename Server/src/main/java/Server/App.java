ea6020eb169fd42d135244e1df9f287118483783
Kintis Alexandros <kintisalexandros@gmail.com>
2021-04-21 01:28:20 +0300

    Added changes

--- Server/src/main/java/Server/App.java
+++ Server/src/main/java/Server/App.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 
 public class App{
 
-	private static ArrayList<VideoFile> files;
+	private ArrayList<VideoFile> files;
     public static final String folderName = "videos";
 
 	public static void main(String[] args) {
+		new App().startProcess();
+	}
+
+	private void startProcess() {
+        AppLogger.log(AppLogger.LogLevel.INFO, "Sequence Starting");
 
 		FilesHandler handler = new FilesHandler();
 		files = handler.getFiles();
-
 	}
-
-
 
 }
